37b8ccc90ab8075a162f245f5e0a9f203250b23d
Prof. Wanderson Timóteo <wanderson_timoteo@outlook.com>
2024-09-24 11:30:31 -0400

    add exemplo de atributos em POO

---
+++ 02-atributos.js
@@ -0,0 +1,12 @@
+class Pessoa {
+    // Atributo de classe
+    static especie = "Humano";
+
+    constructor(nome, idade, altura, peso) {
+        // Inicialização dos atributos
+        this.nome = nome; // Atributo de instância
+        this.idade = idade; // Atributo de instância
+        this.altura = altura; // Atributo de instância
+        this.peso = peso; // Atributo de instância
+    }
+}
